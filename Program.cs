559c5ebcc461e047f4afd728d1e555046e96b935
F0sya <nikitachmyria@gmail.com>
2024-11-12 20:07:20 +0200

    First task is completed.

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ namespace intro_c_
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
             Console.WriteLine("It's easy to win forgiveness for being wrong;\nbeing right is what gets you into real trouble.\nBjarne Stroustrup") ;
