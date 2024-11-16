07fc69f26229db2d8d3934209a1f43930b64a92f
F0sya <nikitachmyria@gmail.com>
2024-11-16 12:13:25 +0200

    Third hometask is completed

--- Program.cs
+++ Program.cs
@@ -7,12 +7,15 @@ namespace intro_c_
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter a number:");
-            double number = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter a percent:");
-            int percent = Convert.ToInt32(Console.ReadLine());
+            string number;
+            string result = "";
+            for (int i =0; i < 4; i++)
+            {
+                number = Console.ReadLine();
 
-            Console.WriteLine((number/100) * percent);
+                result = result + number;
+            }
+            Console.WriteLine(Convert.ToInt32(result));
         }
     }
 }
