de2a7f61a2d8c87afc703e49862e06cb6d680ed7
F0sya <nikitachmyria@gmail.com>
2024-11-16 11:36:55 +0200

    First hometask is completed

--- Program.cs
+++ Program.cs
@@ -7,26 +7,30 @@ namespace intro_c_
     {
         static void Main(string[] args)
         {
-            Console.Write("Horizontal(1) or Vertical(2)?");
-            int choice = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Length of line:");
-            int length = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Symbol:");
-            char symbol = Convert.ToChar(Console.ReadLine());
-            switch (choice)
+            try
             {
-                case 1:
-                    for(int i = 0; i < length; i++)
-                    {
-                        Console.Write(symbol);
-                    }
-                    break;
-                case 2:
-                    for (int i = 0; i < length; i++)
-                    {
-                        Console.WriteLine(symbol);
-                    }
-                    break;
+                Console.WriteLine("Enter a number:");
+                int number = Convert.ToInt32(Console.ReadLine());
+                if (number < 0 || number > 100)
+                {
+                    throw new Exception("Number need to be 0 < x < 100;");
+                }
+                if (number % 3 == 0)
+                {
+                    Console.Write("Fizz");
+                }
+                if (number % 5 == 0)
+                {
+                    Console.Write("Buzz");
+                }
+                else if (number % 3 != 0) 
+                {
+                    Console.Write(number);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
             }
         }
     }
