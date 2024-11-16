431cd1f3fee9c762737b65771f6099d03c75fc42
F0sya <nikitachmyria@gmail.com>
2024-11-16 12:04:34 +0200

    Second hometask is completed

--- Program.cs
+++ Program.cs
@@ -7,7 +7,12 @@ namespace intro_c_
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("It's easy to win forgiveness for being wrong;\nbeing right is what gets you into real trouble.\nBjarne Stroustrup") ;
+            Console.Write("Enter a number:");
+            double number = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter a percent:");
+            int percent = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine((number/100) * percent);
         }
     }
 }
